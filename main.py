b39e3d61872327a6fe282fc97872645fb39fb01c
Wildan Yazid Ziddan <wildanziddan28@gmail.com>
2026-06-12 21:29:55 +0700

    fix: allow vercel domain in cors policy

--- main.py
+++ main.py
@@ -17,7 +17,11 @@ app = FastAPI(title="Melanolens - API Backend")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "https://melanolens.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
