24279336a67043dd926dcb6fe69161d4233ee5bd
Wildan Yazid Ziddan <wildanziddan28@gmail.com>
2026-09-10 12:30:55 +0700

    fix: allow all origins in CORS middleware

--- main.py
+++ main.py
@@ -19,11 +19,7 @@ app = FastAPI(title="Melanolens - API Backend")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
-        "https://melanolens.vercel.app"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
